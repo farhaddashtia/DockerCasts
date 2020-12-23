14e2636cd2cee23d314f8cc471e6082933a8ac96
Farhad Dashti Ardakani <farhaddashtia@gmail.com>
2020-12-23 00:57:56 -0800

    Changed the port number and the message showing up on the web-page

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('How are you doing');
+  res.send('How are you doing? This is the first web application that we made using Docker!');
 });
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
 });
